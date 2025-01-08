eb74159c24b400b540b89c60acee7886aad7b68a
desc0059 <ldescotes1@gmail.com>
2025-01-08 18:28:01 +0100

    A -> to Link

--- src/app/components/nav/index.tsx
+++ src/app/components/nav/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import DateNow from "../ux/date"
 
 const Nav = () => {
@@ -8,18 +9,18 @@ const Nav = () => {
                     <p className="z-50 cursor-auto">Louis Descôtes</p>
                 </div>
                 <div className="absolute flex right-0 pr-10 lg:pr-20">
-                    <a className="z-50 cursor-pointer" href="/about">About</a>
+                    <Link className="z-50 cursor-pointer" href="/about">About</Link>
                 </div>
             </aside>
             <aside className="relative left-0 bottom-4 w-full h-full flex items-end ">
                 <DateNow />
                 <div className="absolute w-full flex justify-center">
-                    <a className="z-50 cursor-pointer" href="email:ldescotes1@gmail.com">Contact</a>
+                    <Link className="z-50 cursor-pointer" href="mailto:ldescotes1@gmail.com">Contact</Link>
                 </div>
                 <div className="absolute gap-1 left-0 flex items-end justify-end w-full pr-10 lg:pr-20">
-                    <a className="z-50 cursor-pointer" href="/">X</a>
+                    <Link className="z-50 cursor-pointer" href="https://x.com/ldescotes1">X</Link>
                     <p>-</p>
-                    <a className="z-50 cursor-pointer" href="/">Linkedin</a>
+                    <Link className="z-50 cursor-pointer" href="https://www.linkedin.com/in/louis-descotes/">Linkedin</Link>
                 </div>
             </aside>
         </section>
