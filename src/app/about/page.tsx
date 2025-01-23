c3ef9c17648596b13f403226e846c30f745bcd90
desc0059 <ldescotes1@gmail.com>
2025-01-23 15:38:41 +0100

    on recommence tout

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function About() {
-    return (
-      <section className="col-start-2 col-end-5 row-start-5 row-end-6 lg:col-start-2 lg:col-end-5 lg:row-start-3 lg:row-end-4 h-full flex items-center ">
-        <p className="z-50">Hello, moi c’est Louis, un alternant de 2e année de BUT MMI. <br />
-        Passionné par le web et le design, je m’inspire des sites primés pour recréer et réinventer des effets interactifs captivant qui enrichissent l’expérience utilisateur.</p>
-      </section>
-    );
-  }
-  
