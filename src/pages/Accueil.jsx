814b22f51334562c3cef4b5fec67688d83e5a539
desc0059 <ldescotes1@gmail.com>
2025-01-24 19:29:51 +0100

    little ajustement

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -98,7 +98,7 @@ const Accueil = () => {
               setHoveredLink(null);
             }}
             to="/explorations"
-            className=" h-full flex flex-col lg:flex-row lg:justify-between lg:items-center w-full "
+            className=" h-full flex flex-col lg:flex-row justify-center lg:justify-between lg:items-center w-full "
           >
             <div className="flex items-center relative">
               <p
