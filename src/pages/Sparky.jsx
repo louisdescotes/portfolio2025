fca9bcc703d0861562c23d786586fccf0d1de2c9
desc0059 <ldescotes1@gmail.com>
2025-01-25 17:35:42 +0100

    fix autoscreen on mobile

--- src/pages/Sparky.jsx
+++ src/pages/Sparky.jsx
@@ -10,7 +10,7 @@ const Sparky = () => {
                 </article>
             </section>
             <section className="flex flex-col gap-16 pb-32">
-            <video playsinline autoplay muted controls>
+            <video playsinline autoPlay muted controls>
             <source src="/img/sparky/sparky.mp4" type="video/mp4"/>
                 </video>
                 <img className="w-full" src="/img/sparky/sparky1.jpg" alt="" />
